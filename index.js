016e8156dc73b5d857608b96b6d0fc39dd7deb7c
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2022-01-02 14:19:47 -0700

    adding view employee and view roles

--- index.js
+++ index.js
@@ -62,3 +62,22 @@ function viewDepartments() {
   });
 
 };
+
+function viewEmployees() {
+
+  db.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT (manager.first_name, " ", manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id', function (err, results) {
+    console.table(results);
+    Options();
+
+  })
+};
+
+function viewRoles() {
+  db.query("SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;", function (err, results) {
+    console.table(results);
+    if (err) {
+      console.log(err);
+    };
+    Options();
+  })
+};
